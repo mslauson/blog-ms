1107350961191a61025aa3621149a02f84091728
Matthew Slauson <matthew@slauson.io>
2023-04-03 22:13:06 -0500

    fix more of controller package

--- controller/user_test.go
+++ controller/user_test.go
@@ -3,7 +3,9 @@ package controller
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"gitea.slauson.io/slausonio/go-types/siogeneric"
+	"github.com/stretchr/testify/assert"
 
 	"io"
 	"net/http"
@@ -54,55 +56,53 @@ func MockJson(c *gin.Context, content interface{}, method string) {
 }
 
 func TestListUsers(t *testing.T) {
+
+	uc, ms, _ := initController(t)
+
 	var (
 		w    = httptest.NewRecorder()
 		c, _ = gin.CreateTestContext(w)
 	)
+	ms.On("ListUsers").Return(mUserList, nil)
+	uc.ListUsers(c)
+
+	assert.Truef(t, c.Errors == nil, "c.Errors should be nil")
+}
+
+func TestListUsersError(t *testing.T) {
 
 	uc, ms, _ := initController(t)
-	tests := []struct {
-		name   string
-		want   *siogeneric.AwlistResponse
-		status int
-	}{
-		{name: "happy", want: mUserList, status: 200},
-		{name: "service error", want: mUserList, status: 200},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			ms.On("ListUsers").Return(tt.want)
-			uc.ListUsers(c)
 
-			if w.Code != tt.status {
-				t.Errorf("ListUsers() = %v, want %v", w.Code, tt.status)
-				return
-			}
-		})
-	}
+	var (
+		w    = httptest.NewRecorder()
+		c, _ = gin.CreateTestContext(w)
+	)
+	ms.On("ListUsers").Return(mUserList, errors.New("asdf"))
+	uc.ListUsers(c)
+
+	assert.Truef(t, c.Errors != nil, "c.Errors shouldnt be nil")
 }
 
 func TestUserController_CreateUser(t *testing.T) {
 	tests := []struct {
 		name    string
 		request *siogeneric.AwCreateUserRequest
-		status  int
 		result  *siogeneric.AwUser
 	}{
-		{name: "happy", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusOK, result: mAwUserPtr},
-		{name: "service failure", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusOK, result: nil},
-		{name: "Bad Request - No Email", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Request - bad Email", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "a(", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Request - bad too long", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@.com", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
-		{name: "No Password", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: ""}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Password Missing Number", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^"}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Password Missing Upper", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "attesting&*^1"}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Password Missing Special", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "Mattesting1"}, status: http.StatusBadRequest, result: nil},
-		{name: "Short Password", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "Ma"}, status: http.StatusBadRequest, result: nil},
-		{name: "No Name", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
-		{name: "long Name", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
-		{name: "No Phone", request: &siogeneric.AwCreateUserRequest{Phone: "", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Phone short", request: &siogeneric.AwCreateUserRequest{Phone: "131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Phone long", request: &siogeneric.AwCreateUserRequest{Phone: "212121123123122131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
+		{name: "happy", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, result: mAwUserPtr},
+		{name: "Bad Request - No Email", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "", Name: "b", Password: "MattTesting&*^1"}, result: nil},
+		{name: "Bad Request - bad Email", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "a(", Name: "b", Password: "MattTesting&*^1"}, result: nil},
+		{name: "Bad Request - bad too long", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@.com", Name: "b", Password: "MattTesting&*^1"}, result: nil},
+		{name: "No Password", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: ""}, result: nil},
+		{name: "Bad Password Missing Number", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^"}, result: nil},
+		{name: "Bad Password Missing Upper", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "attesting&*^1"}, result: nil},
+		{name: "Bad Password Missing Special", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "Mattesting1"}, result: nil},
+		{name: "Short Password", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "Ma"}, result: nil},
+		{name: "No Name", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "", Password: "MattTesting&*^1"}, result: nil},
+		{name: "long Name", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", Password: "MattTesting&*^1"}, result: nil},
+		{name: "No Phone", request: &siogeneric.AwCreateUserRequest{Phone: "", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, result: nil},
+		{name: "Bad Phone short", request: &siogeneric.AwCreateUserRequest{Phone: "131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, result: nil},
+		{name: "Bad Phone long", request: &siogeneric.AwCreateUserRequest{Phone: "212121123123122131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, result: nil},
 	}
 
 	for _, tt := range tests {
@@ -124,16 +124,43 @@ func TestUserController_CreateUser(t *testing.T) {
 			}
 
 			MockJson(c, tt.request, "POST")
-			if tt.status == http.StatusOK {
-				ms.On("CreateUser", mock.AnythingOfType("*siogeneric.AwCreateUserRequest")).Return(tt.result)
+			if tt.result != nil {
+				ms.On("CreateUser", mock.AnythingOfType("*siogeneric.AwCreateUserRequest")).Return(tt.result, nil)
 			}
 			uc.CreateUser(c)
-			if w.Code != tt.status {
-				t.Errorf("CreateUser() = %v, want %v", w.Code, tt.status)
-				return
+			if tt.result != nil {
+				assert.Truef(t, c.Errors == nil, "c.Errors should be nil")
+			} else {
+				assert.Truef(t, c.Errors != nil, "c.Errors shouldnt be nil")
 			}
 		})
 	}
+}
+
+func TestUserController_CreateUserServiceFailure(t *testing.T) {
+	request := &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}
+	var (
+		w    = httptest.NewRecorder()
+		c, _ = gin.CreateTestContext(w)
+	)
+	c.Request = &http.Request{
+		Header: make(http.Header),
+	}
+
+	uc, ms, eu := initController(t)
+
+	err := eu.EncryptInterface(request, false)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	MockJson(c, request, "POST")
+	ms.On("CreateUser", mock.AnythingOfType("*siogeneric.AwCreateUserRequest")).Return(mAwUserPtr, errors.New("error"))
+	uc.CreateUser(c)
+
+	assert.Truef(t, c.Errors != nil, "c.Errors shouldn't be nil")
+
 }
 
 func TestUserController_UpdatePassword(t *testing.T) {
@@ -144,7 +171,6 @@ func TestUserController_UpdatePassword(t *testing.T) {
 		result  *siogeneric.AwUser
 	}{
 		{name: "happy", request: &siogeneric.UpdatePasswordRequest{Password: "Mm112a23!"}, status: http.StatusOK, result: mAwUserPtr},
-		{name: "service failure", request: &siogeneric.UpdatePasswordRequest{Password: "Mm112a23!"}, status: http.StatusOK, result: nil},
 		{name: "No Password", request: &siogeneric.UpdatePasswordRequest{Password: ""}, status: http.StatusBadRequest, result: nil},
 		{name: "Bad Password Missing Number", request: &siogeneric.UpdatePasswordRequest{Password: "Mmsdfsdfafsdf!"}, status: http.StatusBadRequest, result: nil},
 		{name: "Bad Password Missing Upper", request: &siogeneric.UpdatePasswordRequest{Password: "aaam112a23!"}, status: http.StatusBadRequest, result: nil},
@@ -173,30 +199,59 @@ func TestUserController_UpdatePassword(t *testing.T) {
 			}
 
 			MockJson(c, tt.request, "PUT")
-			if tt.status == http.StatusOK {
-				ms.On("UpdatePassword", "a", mock.AnythingOfType("*siogeneric.UpdatePasswordRequest")).Return(tt.result)
+			if tt.result != nil {
+				ms.On("UpdatePassword", "a", mock.AnythingOfType("*siogeneric.UpdatePasswordRequest")).Return(tt.result, nil)
 			}
 			uc.UpdatePassword(c)
-			if w.Code != tt.status {
-				t.Errorf("UpdatePassword() = %v, want %v", w.Code, tt.status)
-				return
+			if tt.result != nil {
+				assert.Truef(t, c.Errors == nil, "c.Errors should be nil")
+			} else {
+				assert.Truef(t, c.Errors != nil, "c.Errors shouldnt be nil")
 			}
 		})
 	}
 }
+func TestUserController_UpdatePasswordServiceFailure(t *testing.T) {
+	request := &siogeneric.UpdatePasswordRequest{Password: "Mm112a23!"}
+	var (
+		w    = httptest.NewRecorder()
+		c, _ = gin.CreateTestContext(w)
+	)
+	c.Request = &http.Request{
+		Header: make(http.Header),
+	}
 
+	c.Request = &http.Request{
+		Header: make(http.Header),
+	}
+
+	c.Params = gin.Params{gin.Param{Key: "id", Value: "a"}}
+
+	uc, ms, eu := initController(t)
+
+	err := eu.EncryptInterface(request, false)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	MockJson(c, request, "PUT")
+	ms.On("UpdatePassword", "a", mock.AnythingOfType("*siogeneric.UpdatePasswordRequest")).Return(mAwUserPtr, errors.New("error"))
+	uc.UpdatePassword(c)
+
+	assert.Truef(t, c.Errors != nil, "c.Errors shouldn't be nil")
+
+}
 func TestUserController_UpdateEmail(t *testing.T) {
 	tests := []struct {
 		name    string
 		request *siogeneric.UpdateEmailRequest
-		status  int
 		result  *siogeneric.AwUser
 	}{
-		{name: "happy", request: &siogeneric.UpdateEmailRequest{Email: "fake@fake.com"}, status: http.StatusOK, result: mAwUserPtr},
-		{name: "service failure", request: &siogeneric.UpdateEmailRequest{Email: "fake@fake.com"}, status: http.StatusOK, result: nil},
-		{name: "No Email", request: &siogeneric.UpdateEmailRequest{Email: ""}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Email ", request: &siogeneric.UpdateEmailRequest{Email: "fakefake.com"}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Email too long", request: &siogeneric.UpdateEmailRequest{Email: "\"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@.com"}, status: http.StatusBadRequest, result: nil},
+		{name: "happy", request: &siogeneric.UpdateEmailRequest{Email: "fake@fake.com"}, result: mAwUserPtr},
+		{name: "No Email", request: &siogeneric.UpdateEmailRequest{Email: ""}, result: nil},
+		{name: "Bad Email ", request: &siogeneric.UpdateEmailRequest{Email: "fakefake.com"}, result: nil},
+		{name: "Bad Email too long", request: &siogeneric.UpdateEmailRequest{Email: "\"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@.com"}, result: nil},
 	}
 
 	for _, tt := range tests {
@@ -220,30 +275,62 @@ func TestUserController_UpdateEmail(t *testing.T) {
 			}
 
 			MockJson(c, tt.request, "PUT")
-			if tt.status == http.StatusOK {
-				ms.On("UpdateEmail", "a", mock.AnythingOfType("*siogeneric.UpdateEmailRequest")).Return(tt.result)
+			if tt.result != nil {
+				ms.On("UpdateEmail", "a", mock.AnythingOfType("*siogeneric.UpdateEmailRequest")).Return(tt.result, nil)
 			}
 			uc.UpdateEmail(c)
-			if w.Code != tt.status {
-				t.Errorf("UpdateEmail() = %v, want %v", w.Code, tt.status)
-				return
+			if tt.result != nil {
+				assert.Truef(t, c.Errors == nil, "c.Errors should be nil")
+			} else {
+				assert.Truef(t, c.Errors != nil, "c.Errors shouldnt be nil")
 			}
 		})
 	}
+}
+
+func TestUserController_UpdateEmailServiceFailure(t *testing.T) {
+	request := &siogeneric.UpdateEmailRequest{Email: "a@abc.com"}
+
+	var (
+		w    = httptest.NewRecorder()
+		c, _ = gin.CreateTestContext(w)
+	)
+	c.Request = &http.Request{
+		Header: make(http.Header),
+	}
+
+	c.Request = &http.Request{
+		Header: make(http.Header),
+	}
+
+	c.Params = gin.Params{gin.Param{Key: "id", Value: "a"}}
+
+	uc, ms, eu := initController(t)
+
+	err := eu.EncryptInterface(request, false)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	MockJson(c, request, "PUT")
+	ms.On("UpdateEmail", "a", mock.AnythingOfType("*siogeneric.UpdateEmailRequest")).Return(mAwUserPtr, errors.New("error"))
+	uc.UpdateEmail(c)
+
+	assert.Truef(t, c.Errors != nil, "c.Errors shouldn't be nil")
+
 }
 
 func TestUserController_UpdatePhone(t *testing.T) {
 	tests := []struct {
 		name    string
 		request *siogeneric.UpdatePhoneRequest
-		status  int
 		result  *siogeneric.AwUser
 	}{
-		{name: "happy", request: &siogeneric.UpdatePhoneRequest{Number: "1239323939"}, status: http.StatusOK, result: mAwUserPtr},
-		{name: "service failure", request: &siogeneric.UpdatePhoneRequest{Number: "1239323939"}, status: http.StatusOK, result: nil},
-		{name: "No Phone", request: &siogeneric.UpdatePhoneRequest{Number: ""}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Phone too short ", request: &siogeneric.UpdatePhoneRequest{Number: "23423"}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Phone too long", request: &siogeneric.UpdatePhoneRequest{Number: "2934830298420394809238402893"}, status: http.StatusBadRequest, result: nil},
+		{name: "happy", request: &siogeneric.UpdatePhoneRequest{Number: "1239323939"}, result: mAwUserPtr},
+		{name: "No Phone", request: &siogeneric.UpdatePhoneRequest{Number: ""}, result: nil},
+		{name: "Bad Phone too short ", request: &siogeneric.UpdatePhoneRequest{Number: "23423"}, result: nil},
+		{name: "Bad Phone too long", request: &siogeneric.UpdatePhoneRequest{Number: "2934830298420394809238402893"}, result: nil},
 	}
 
 	for _, tt := range tests {
@@ -267,50 +354,78 @@ func TestUserController_UpdatePhone(t *testing.T) {
 			}
 
 			MockJson(c, tt.request, "PUT")
-			if tt.status == http.StatusOK {
-				ms.On("UpdatePhone", "a", mock.AnythingOfType("*siogeneric.UpdatePhoneRequest")).Return(tt.result)
+			if tt.result != nil {
+				ms.On("UpdatePhone", "a", mock.AnythingOfType("*siogeneric.UpdatePhoneRequest")).Return(tt.result, nil)
 			}
+
 			uc.UpdatePhone(c)
-			if w.Code != tt.status {
-				t.Errorf("UpdatePhone() = %v, want %v", w.Code, tt.status)
-				return
+			if tt.result != nil {
+				assert.Truef(t, c.Errors == nil, "c.Errors should be nil")
+			} else {
+				assert.Truef(t, c.Errors != nil, "c.Errors shouldnt be nil")
 			}
 		})
 	}
 }
+func TestUserController_UpdatePhoneServiceFailure(t *testing.T) {
+	request := &siogeneric.UpdatePhoneRequest{Number: "3647586976"}
 
-func TestGetUserById(t *testing.T) {
 	var (
 		w    = httptest.NewRecorder()
 		c, _ = gin.CreateTestContext(w)
 	)
 
+	c.Request = &http.Request{
+		Header: make(http.Header),
+	}
+
+	c.Request = &http.Request{
+		Header: make(http.Header),
+	}
+
+	c.Params = gin.Params{gin.Param{Key: "id", Value: "a"}}
+
+	uc, ms, eu := initController(t)
+
+	err := eu.EncryptInterface(request, false)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	MockJson(c, request, "PUT")
+	ms.On("UpdatePhone", "a", mock.AnythingOfType("*siogeneric.UpdatePhoneRequest")).Return(mAwUserPtr, errors.New("error"))
+	uc.UpdatePhone(c)
+
+	assert.Truef(t, c.Errors != nil, "c.Errors shouldn't be nil")
+
+}
+func TestGetUserById(t *testing.T) {
+
 	uc, ms, _ := initController(t)
-	tests := []struct {
-		name   string
-		want   *siogeneric.AwUser
-		status int
-	}{
-		{name: "happy", want: mAwUserPtr, status: 200},
-		{name: "service error", want: nil, status: 200},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c.Request = &http.Request{
-				Header: make(http.Header),
-			}
 
-			c.Params = gin.Params{gin.Param{Key: "id", Value: "a"}}
+	var (
+		w    = httptest.NewRecorder()
+		c, _ = gin.CreateTestContext(w)
+	)
+	ms.On("GetUserById", "a").Return(mAwUserPtr, nil)
+	uc.GetUserById(c)
 
-			ms.On("GetUserByID", "a").Return(tt.want)
-			uc.GetUserById(c)
+	assert.Truef(t, c.Errors == nil, "c.Errors should be nil")
+}
 
-			if w.Code != tt.status {
-				t.Errorf("GetUserById()() = %v, want %v", w.Code, tt.status)
-				return
-			}
-		})
-	}
+func TestGetUserByIdError(t *testing.T) {
+
+	uc, ms, _ := initController(t)
+
+	var (
+		w    = httptest.NewRecorder()
+		c, _ = gin.CreateTestContext(w)
+	)
+	ms.On("GetUserById", "a").Return(nil, errors.New("asdf"))
+	uc.GetUserById(c)
+
+	assert.Truef(t, c.Errors != nil, "c.Errors shouldnt be nil")
 }
 
 func TestDeleteUser(t *testing.T) {
@@ -325,8 +440,8 @@ func TestDeleteUser(t *testing.T) {
 		want   siogeneric.SuccessResponse
 		status int
 	}{
-		{name: "happy", want: siogeneric.SuccessResponse{Success: true}, status: 200},
-		{name: "service error", want: siogeneric.SuccessResponse{Success: false}, status: 200},
+		{name: "happy", want: siogeneric.SuccessResponse{Success: true}},
+		{name: "service error", want: siogeneric.SuccessResponse{Success: false}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
