0119c5177c58c8110b13a258a2282c30c0e2d6eb
Matthew Slauson <matthew@slauson.io>
2023-04-03 19:33:53 -0500

    somewhat fix

--- controller/user_test.go
+++ controller/user_test.go
@@ -3,28 +3,28 @@ package controller
 import (
 	"bytes"
 	"encoding/json"
+	"gitea.slauson.io/slausonio/go-types/siogeneric"
+
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	sioModelGeneric "gitea.slauson.io/slausonio/go-libs/model/generic"
 	"gitea.slauson.io/slausonio/go-utils/sioUtils"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/mock"
 
-	sioModel "gitea.slauson.io/slausonio/go-libs/model"
 	"gitea.slauson.io/slausonio/iam-ms/service/mocks"
 )
 
 var (
-	mAwUser = sioModel.AwUser{
+	mAwUser = siogeneric.AwUser{
 		Email: "t@t.com",
 	}
 	mAwUserPtr = &mAwUser
-	mUserList  = &sioModel.AwlistResponse{
+	mUserList  = &siogeneric.AwlistResponse{
 		Total: 1,
-		Users: []sioModel.AwUser{mAwUser},
+		Users: []siogeneric.AwUser{mAwUser},
 	}
 )
 
@@ -62,7 +62,7 @@ func TestListUsers(t *testing.T) {
 	uc, ms, _ := initController(t)
 	tests := []struct {
 		name   string
-		want   *sioModel.AwlistResponse
+		want   *siogeneric.AwlistResponse
 		status int
 	}{
 		{name: "happy", want: mUserList, status: 200},
@@ -70,7 +70,7 @@ func TestListUsers(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			ms.On("ListUsers", c).Return(tt.want)
+			ms.On("ListUsers").Return(tt.want)
 			uc.ListUsers(c)
 
 			if w.Code != tt.status {
@@ -84,25 +84,25 @@ func TestListUsers(t *testing.T) {
 func TestUserController_CreateUser(t *testing.T) {
 	tests := []struct {
 		name    string
-		request *sioModel.AwCreateUserRequest
+		request *siogeneric.AwCreateUserRequest
 		status  int
-		result  *sioModel.AwUser
+		result  *siogeneric.AwUser
 	}{
-		{name: "happy", request: &sioModel.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusOK, result: mAwUserPtr},
-		{name: "service failure", request: &sioModel.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusOK, result: nil},
-		{name: "Bad Request - No Email", request: &sioModel.AwCreateUserRequest{Phone: "2121212131", Email: "", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Request - bad Email", request: &sioModel.AwCreateUserRequest{Phone: "2121212131", Email: "a(", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Request - bad too long", request: &sioModel.AwCreateUserRequest{Phone: "2121212131", Email: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@.com", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
-		{name: "No Password", request: &sioModel.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: ""}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Password Missing Number", request: &sioModel.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^"}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Password Missing Upper", request: &sioModel.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "attesting&*^1"}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Password Missing Special", request: &sioModel.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "Mattesting1"}, status: http.StatusBadRequest, result: nil},
-		{name: "Short Password", request: &sioModel.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "Ma"}, status: http.StatusBadRequest, result: nil},
-		{name: "No Name", request: &sioModel.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
-		{name: "long Name", request: &sioModel.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
-		{name: "No Phone", request: &sioModel.AwCreateUserRequest{Phone: "", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Phone short", request: &sioModel.AwCreateUserRequest{Phone: "131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Phone long", request: &sioModel.AwCreateUserRequest{Phone: "212121123123122131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
+		{name: "happy", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusOK, result: mAwUserPtr},
+		{name: "service failure", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusOK, result: nil},
+		{name: "Bad Request - No Email", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
+		{name: "Bad Request - bad Email", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "a(", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
+		{name: "Bad Request - bad too long", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@.com", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
+		{name: "No Password", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: ""}, status: http.StatusBadRequest, result: nil},
+		{name: "Bad Password Missing Number", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^"}, status: http.StatusBadRequest, result: nil},
+		{name: "Bad Password Missing Upper", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "attesting&*^1"}, status: http.StatusBadRequest, result: nil},
+		{name: "Bad Password Missing Special", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "Mattesting1"}, status: http.StatusBadRequest, result: nil},
+		{name: "Short Password", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "b", Password: "Ma"}, status: http.StatusBadRequest, result: nil},
+		{name: "No Name", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
+		{name: "long Name", request: &siogeneric.AwCreateUserRequest{Phone: "2121212131", Email: "t@t.com", Name: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
+		{name: "No Phone", request: &siogeneric.AwCreateUserRequest{Phone: "", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
+		{name: "Bad Phone short", request: &siogeneric.AwCreateUserRequest{Phone: "131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
+		{name: "Bad Phone long", request: &siogeneric.AwCreateUserRequest{Phone: "212121123123122131", Email: "t@t.com", Name: "b", Password: "MattTesting&*^1"}, status: http.StatusBadRequest, result: nil},
 	}
 
 	for _, tt := range tests {
@@ -125,7 +125,7 @@ func TestUserController_CreateUser(t *testing.T) {
 
 			MockJson(c, tt.request, "POST")
 			if tt.status == http.StatusOK {
-				ms.On("CreateUser", mock.AnythingOfType("*sioModel.AwCreateUserRequest"), c).Return(tt.result)
+				ms.On("CreateUser", mock.AnythingOfType("*siogeneric.AwCreateUserRequest")).Return(tt.result)
 			}
 			uc.CreateUser(c)
 			if w.Code != tt.status {
@@ -139,17 +139,17 @@ func TestUserController_CreateUser(t *testing.T) {
 func TestUserController_UpdatePassword(t *testing.T) {
 	tests := []struct {
 		name    string
-		request *sioModel.UpdatePasswordRequest
+		request *siogeneric.UpdatePasswordRequest
 		status  int
-		result  *sioModel.AwUser
+		result  *siogeneric.AwUser
 	}{
-		{name: "happy", request: &sioModel.UpdatePasswordRequest{Password: "Mm112a23!"}, status: http.StatusOK, result: mAwUserPtr},
-		{name: "service failure", request: &sioModel.UpdatePasswordRequest{Password: "Mm112a23!"}, status: http.StatusOK, result: nil},
-		{name: "No Password", request: &sioModel.UpdatePasswordRequest{Password: ""}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Password Missing Number", request: &sioModel.UpdatePasswordRequest{Password: "Mmsdfsdfafsdf!"}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Password Missing Upper", request: &sioModel.UpdatePasswordRequest{Password: "aaam112a23!"}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Password Missing Special", request: &sioModel.UpdatePasswordRequest{Password: "Mm112a2333"}, status: http.StatusBadRequest, result: nil},
-		{name: "Short Password", request: &sioModel.UpdatePasswordRequest{Password: "Mm1^"}, status: http.StatusBadRequest, result: nil},
+		{name: "happy", request: &siogeneric.UpdatePasswordRequest{Password: "Mm112a23!"}, status: http.StatusOK, result: mAwUserPtr},
+		{name: "service failure", request: &siogeneric.UpdatePasswordRequest{Password: "Mm112a23!"}, status: http.StatusOK, result: nil},
+		{name: "No Password", request: &siogeneric.UpdatePasswordRequest{Password: ""}, status: http.StatusBadRequest, result: nil},
+		{name: "Bad Password Missing Number", request: &siogeneric.UpdatePasswordRequest{Password: "Mmsdfsdfafsdf!"}, status: http.StatusBadRequest, result: nil},
+		{name: "Bad Password Missing Upper", request: &siogeneric.UpdatePasswordRequest{Password: "aaam112a23!"}, status: http.StatusBadRequest, result: nil},
+		{name: "Bad Password Missing Special", request: &siogeneric.UpdatePasswordRequest{Password: "Mm112a2333"}, status: http.StatusBadRequest, result: nil},
+		{name: "Short Password", request: &siogeneric.UpdatePasswordRequest{Password: "Mm1^"}, status: http.StatusBadRequest, result: nil},
 	}
 
 	for _, tt := range tests {
@@ -174,7 +174,7 @@ func TestUserController_UpdatePassword(t *testing.T) {
 
 			MockJson(c, tt.request, "PUT")
 			if tt.status == http.StatusOK {
-				ms.On("UpdatePassword", "a", mock.AnythingOfType("*sioModel.UpdatePasswordRequest"), c).Return(tt.result)
+				ms.On("UpdatePassword", "a", mock.AnythingOfType("*siogeneric.UpdatePasswordRequest")).Return(tt.result)
 			}
 			uc.UpdatePassword(c)
 			if w.Code != tt.status {
@@ -188,15 +188,15 @@ func TestUserController_UpdatePassword(t *testing.T) {
 func TestUserController_UpdateEmail(t *testing.T) {
 	tests := []struct {
 		name    string
-		request *sioModel.UpdateEmailRequest
+		request *siogeneric.UpdateEmailRequest
 		status  int
-		result  *sioModel.AwUser
+		result  *siogeneric.AwUser
 	}{
-		{name: "happy", request: &sioModel.UpdateEmailRequest{Email: "fake@fake.com"}, status: http.StatusOK, result: mAwUserPtr},
-		{name: "service failure", request: &sioModel.UpdateEmailRequest{Email: "fake@fake.com"}, status: http.StatusOK, result: nil},
-		{name: "No Email", request: &sioModel.UpdateEmailRequest{Email: ""}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Email ", request: &sioModel.UpdateEmailRequest{Email: "fakefake.com"}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Email too long", request: &sioModel.UpdateEmailRequest{Email: "\"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@.com"}, status: http.StatusBadRequest, result: nil},
+		{name: "happy", request: &siogeneric.UpdateEmailRequest{Email: "fake@fake.com"}, status: http.StatusOK, result: mAwUserPtr},
+		{name: "service failure", request: &siogeneric.UpdateEmailRequest{Email: "fake@fake.com"}, status: http.StatusOK, result: nil},
+		{name: "No Email", request: &siogeneric.UpdateEmailRequest{Email: ""}, status: http.StatusBadRequest, result: nil},
+		{name: "Bad Email ", request: &siogeneric.UpdateEmailRequest{Email: "fakefake.com"}, status: http.StatusBadRequest, result: nil},
+		{name: "Bad Email too long", request: &siogeneric.UpdateEmailRequest{Email: "\"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@.com"}, status: http.StatusBadRequest, result: nil},
 	}
 
 	for _, tt := range tests {
@@ -221,7 +221,7 @@ func TestUserController_UpdateEmail(t *testing.T) {
 
 			MockJson(c, tt.request, "PUT")
 			if tt.status == http.StatusOK {
-				ms.On("UpdateEmail", "a", mock.AnythingOfType("*sioModel.UpdateEmailRequest"), c).Return(tt.result)
+				ms.On("UpdateEmail", "a", mock.AnythingOfType("*siogeneric.UpdateEmailRequest")).Return(tt.result)
 			}
 			uc.UpdateEmail(c)
 			if w.Code != tt.status {
@@ -235,15 +235,15 @@ func TestUserController_UpdateEmail(t *testing.T) {
 func TestUserController_UpdatePhone(t *testing.T) {
 	tests := []struct {
 		name    string
-		request *sioModel.UpdatePhoneRequest
+		request *siogeneric.UpdatePhoneRequest
 		status  int
-		result  *sioModel.AwUser
+		result  *siogeneric.AwUser
 	}{
-		{name: "happy", request: &sioModel.UpdatePhoneRequest{Number: "1239323939"}, status: http.StatusOK, result: mAwUserPtr},
-		{name: "service failure", request: &sioModel.UpdatePhoneRequest{Number: "1239323939"}, status: http.StatusOK, result: nil},
-		{name: "No Phone", request: &sioModel.UpdatePhoneRequest{Number: ""}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Phone too short ", request: &sioModel.UpdatePhoneRequest{Number: "23423"}, status: http.StatusBadRequest, result: nil},
-		{name: "Bad Phone too long", request: &sioModel.UpdatePhoneRequest{Number: "2934830298420394809238402893"}, status: http.StatusBadRequest, result: nil},
+		{name: "happy", request: &siogeneric.UpdatePhoneRequest{Number: "1239323939"}, status: http.StatusOK, result: mAwUserPtr},
+		{name: "service failure", request: &siogeneric.UpdatePhoneRequest{Number: "1239323939"}, status: http.StatusOK, result: nil},
+		{name: "No Phone", request: &siogeneric.UpdatePhoneRequest{Number: ""}, status: http.StatusBadRequest, result: nil},
+		{name: "Bad Phone too short ", request: &siogeneric.UpdatePhoneRequest{Number: "23423"}, status: http.StatusBadRequest, result: nil},
+		{name: "Bad Phone too long", request: &siogeneric.UpdatePhoneRequest{Number: "2934830298420394809238402893"}, status: http.StatusBadRequest, result: nil},
 	}
 
 	for _, tt := range tests {
@@ -268,7 +268,7 @@ func TestUserController_UpdatePhone(t *testing.T) {
 
 			MockJson(c, tt.request, "PUT")
 			if tt.status == http.StatusOK {
-				ms.On("UpdatePhone", "a", mock.AnythingOfType("*sioModel.UpdatePhoneRequest"), c).Return(tt.result)
+				ms.On("UpdatePhone", "a", mock.AnythingOfType("*siogeneric.UpdatePhoneRequest")).Return(tt.result)
 			}
 			uc.UpdatePhone(c)
 			if w.Code != tt.status {
@@ -288,7 +288,7 @@ func TestGetUserById(t *testing.T) {
 	uc, ms, _ := initController(t)
 	tests := []struct {
 		name   string
-		want   *sioModel.AwUser
+		want   *siogeneric.AwUser
 		status int
 	}{
 		{name: "happy", want: mAwUserPtr, status: 200},
@@ -302,7 +302,7 @@ func TestGetUserById(t *testing.T) {
 
 			c.Params = gin.Params{gin.Param{Key: "id", Value: "a"}}
 
-			ms.On("GetUserByID", "a", c).Return(tt.want)
+			ms.On("GetUserByID", "a").Return(tt.want)
 			uc.GetUserById(c)
 
 			if w.Code != tt.status {
@@ -322,11 +322,11 @@ func TestDeleteUser(t *testing.T) {
 	uc, ms, _ := initController(t)
 	tests := []struct {
 		name   string
-		want   sioModelGeneric.SuccessResponse
+		want   siogeneric.SuccessResponse
 		status int
 	}{
-		{name: "happy", want: sioModelGeneric.SuccessResponse{Success: true}, status: 200},
-		{name: "service error", want: sioModelGeneric.SuccessResponse{Success: false}, status: 200},
+		{name: "happy", want: siogeneric.SuccessResponse{Success: true}, status: 200},
+		{name: "service error", want: siogeneric.SuccessResponse{Success: false}, status: 200},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -336,7 +336,7 @@ func TestDeleteUser(t *testing.T) {
 
 			c.Params = gin.Params{gin.Param{Key: "id", Value: "a"}}
 
-			ms.On("DeleteUser", "a", c).Return(tt.want)
+			ms.On("DeleteUser", "a").Return(tt.want)
 			uc.DeleteUser(c)
 
 			if w.Code != tt.status {
