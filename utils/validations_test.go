2c8e1a4454fb1bb1440977b8d038c9dc8e36e095
Matthew Slauson <matthew@slauson.io>
2023-05-05 14:12:45 -0500

    finish validator tests

--- utils/validations_test.go
+++ utils/validations_test.go
@@ -143,3 +143,176 @@ func TestValidateCreateUserRequest(t *testing.T) {
 		})
 	}
 }
+
+func TestValidateUpdateEmailRequest(t *testing.T) {
+	tests := []struct {
+		name    string
+		request *siogeneric.UpdateEmailRequest
+		error   error
+	}{
+		{
+			name: "Valid",
+			request: &siogeneric.UpdateEmailRequest{
+				Email: "fake.fake@com",
+			},
+			error: nil,
+		},
+		{
+			name: "Missing @",
+			request: &siogeneric.UpdateEmailRequest{
+				Email: "fakefake.com",
+			},
+			error: sioerror.NewSioBadRequestError("invalid email"),
+		},
+		{
+			name: "Missing Domain",
+			request: &siogeneric.UpdateEmailRequest{
+				Email: "fake@fake.",
+			},
+			error: sioerror.NewSioBadRequestError("invalid email"),
+		},
+		{
+			name: "Missing Name",
+			request: &siogeneric.UpdateEmailRequest{
+				Email: "fakefake.com",
+			},
+			error: sioerror.NewSioBadRequestError("invalid email"),
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			v := NewIamValidations()
+			err := v.ValidateUpdateEmailRequest(test.request)
+			if test.error == nil {
+				assert.Nilf(t, err, "Expected no error, got %v", err)
+			} else {
+				assert.Equalf(
+					t,
+					test.error.Error(),
+					err.Error(),
+					"Expected error %s, got %s",
+					test.error.Error(),
+					err.Error(),
+				)
+			}
+		})
+	}
+}
+
+func TestValidateUpdatePassword(t *testing.T) {
+	tests := []struct {
+		name    string
+		request *siogeneric.UpdatePasswordRequest
+		error   error
+	}{
+		{
+			name: "Valid",
+			request: &siogeneric.UpdatePasswordRequest{
+				Password: "Faksdfsdfe@123!",
+			},
+			error: nil,
+		},
+		{
+			name: "Bad Password Missing Number",
+			request: &siogeneric.UpdatePasswordRequest{
+				Password: "Faksdfsdfe@",
+			},
+			error: sioerror.NewSioBadRequestError(
+				"invalid password: Requirements are 8 char min, 1 upper, 1 special, and 1 numerical",
+			),
+		},
+		{
+			name: "Bad Password Missing Upper",
+			request: &siogeneric.UpdatePasswordRequest{
+				Password: "fake@123",
+			},
+			error: sioerror.NewSioBadRequestError(
+				"invalid password: Requirements are 8 char min, 1 upper, 1 special, and 1 numerical",
+			),
+		},
+		{
+			name: "Bad Password Missing Special",
+			request: &siogeneric.UpdatePasswordRequest{
+				Password: "Fake123",
+			},
+			error: sioerror.NewSioBadRequestError(
+				"invalid password: Requirements are 8 char min, 1 upper, 1 special, and 1 numerical",
+			),
+		},
+		{
+			name: "Short Password",
+			request: &siogeneric.UpdatePasswordRequest{
+				Password: "F@123",
+			},
+			error: sioerror.NewSioBadRequestError(
+				"invalid password: Requirements are 8 char min, 1 upper, 1 special, and 1 numerical",
+			),
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			v := NewIamValidations()
+			err := v.ValidateUpdatePasswordRequest(test.request)
+			if test.error == nil {
+				assert.Nilf(t, err, "Expected no error, got %v", err)
+			} else {
+				assert.Equalf(
+					t,
+					test.error.Error(),
+					err.Error(),
+					"Expected error %s, got %s",
+					test.error.Error(),
+					err.Error(),
+				)
+			}
+		})
+	}
+}
+
+func TestValidateUpdatePhone(t *testing.T) {
+	tests := []struct {
+		name    string
+		request *siogeneric.UpdatePhoneRequest
+		error   error
+	}{
+		{
+			name: "Valid",
+			request: &siogeneric.UpdatePhoneRequest{
+				Number: "5555555555",
+			},
+			error: nil,
+		},
+		{
+			name: "bad phone short",
+			request: &siogeneric.UpdatePhoneRequest{
+				Number: "5555555",
+			},
+			error: sioerror.NewSioBadRequestError("please enter a ten digit mobile number"),
+		},
+		{
+			name: "bad phone long",
+			request: &siogeneric.UpdatePhoneRequest{
+				Number: "5555889555555",
+			},
+			error: sioerror.NewSioBadRequestError("please enter a ten digit mobile number"),
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			v := NewIamValidations()
+			err := v.ValidateUpdatePhoneRequest(test.request)
+			if test.error == nil {
+				assert.Nilf(t, err, "Expected no error, got %v", err)
+			} else {
+				assert.Equalf(
+					t,
+					test.error.Error(),
+					err.Error(),
+					"Expected error %s, got %s",
+					test.error.Error(),
+					err.Error(),
+				)
+			}
+		})
+	}
+}
