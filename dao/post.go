90821fef363c14ae49f62645b4488f8805574dc2
Matthew Slauson <matthew@slauson.io>
2023-06-18 14:47:13 -0500

    stop for now

--- dao/post.go
+++ dao/post.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 
-	"entgo.io/ent/entc/integration/edgefield/ent/post"
 	"gitea.slauson.io/slausonio/go-types/siogeneric"
 	"gitea.slauson.io/slausonio/go-utils/siodao"
 )
@@ -58,6 +57,45 @@ func (pd *PDao) Exists(post *siogeneric.BlogPost) (bool, error) {
 func (pd *PDao) ExistsByID(ID int64) (bool, error) {
 	sql := `SELECT EXISTS(SELECT 1 FROM blog_posts WHERE id = $1)`
 	var exists bool
-	err := pd.db.QueryRowContext(ctx, sql, post.ID).Scan(&exists)
+	err := pd.db.QueryRowContext(ctx, sql, ID).Scan(&exists)
 	return exists, err
+}
+
+func (pd *PDao) GetAllPosts() (*[]siogeneric.BlogPost, error) {
+	sql := `SELECT 
+			p.id, p.title, p.body, p.posted_date, p.updated_date, p.deletion_date, p.soft_deleted,
+			c.id, c.content, c.comment_date, c.soft_deleted, c.deletion_date
+		FROM blog.post p
+		LEFT JOIN blog.comment c ON p.id = c.post_id`
+
+	rows, err := pd.db.QueryContext(ctx, sql)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var posts []siogeneric.BlogPost
+	for rows.Next() {
+		var post siogeneric.BlogPost
+		err := rows.Scan(
+			&post.ID,
+			&post.Title,
+			&post.Body,
+			&post.PostedDate,
+			&post.CreatedByID,
+			&post.SoftDeleted,
+		)
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
+	}
+	return &posts, nil
+}
+
+func (pd *PDao) scanPost(row *sql.Row) (*siogeneric.BlogPost, error) {
+	post := new(siogeneric.BlogPost)
+	if err := row.Scan(&post.ID, &post.Title, &post.Body, &post.PostedDate,
+		&post.UpdatedDate, &post.DeletionDate, &post.SoftDeleted); err != nil {
+		return nil, err
+	}
 }
