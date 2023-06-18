c1993288a4ce64a9814fb68c94316e4816f6213a
Matthew Slauson <matthew@slauson.io>
2023-06-18 14:01:50 -0500

    change name and add interface

--- dao/post.go
+++ dao/post.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 
+	"gitea.slauson.io/slausonio/go-types/siogeneric"
 	"gitea.slauson.io/slausonio/go-utils/siodao"
 )
 
@@ -11,6 +12,18 @@ var ctx = context.Background()
 
 type PostDao struct {
 	db *sql.DB
+}
+
+type PostDaoInterface interface {
+	CreatePost(post *siogeneric.BlogPost) error
+	ExistsPost(post *siogeneric.BlogPost) (bool, error)
+	ExistsByIDPost(ID int64) (bool, error)
+	GetAllPosts() (*[]siogeneric.BlogPost, error)
+	UpdatePost(post *siogeneric.BlogPost) error
+	AddCommentToPost(post *siogeneric.BlogPost, comment *siogeneric.BlogComment) error
+	UpdateCommentInPost(post *siogeneric.BlogPost, comment *siogeneric.BlogComment) error
+	SoftDeletePost(post *siogeneric.BlogPost) error
+	SoftDeleteCommentOnPost(post *siogeneric.BlogPost, comment *siogeneric.BlogComment) error
 }
 
 func NewPostDao() *PostDao {
