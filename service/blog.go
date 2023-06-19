d5160a7fc2f55a9be2ce2e83d8a06e3107f20608
Matthew Slauson <matthew@slauson.io>
2023-06-19 16:57:53 -0500

    define interface and implement

--- service/blog.go
+++ service/blog.go
@@ -2,7 +2,7 @@ package service
 
 import (
 	"gitea.slauson.io/blog/blog-ms/dao"
-	"gitea.slauson.io/slausonio/go-types/siogeneric"
+	"gitea.slauson.io/blog/blog-ms/dto"
 )
 
 type BlogSvc struct {
@@ -10,11 +10,52 @@ type BlogSvc struct {
 }
 
 type BlogService interface {
-	CreatePost(post *siogeneric.BlogPost) error
+	GetPost(id int64) (*dto.PostResponse, error)
+
+
+  }
+	  panic("not implemented") // TODO: Implement
+	GetAllPosts() (*[]*dto.PostResponse, error)
+	CreatePost(post *dto.CreatePostRequest) (*dto.PostResponse, error)
+  func (blogsvc *BlogSvc) SoftenDeleteComment(id int64) (*dto.CommentResponse, error) {
+
+    }
+	    panic("not implemented") // TODO: Implement
+      func (blogsvc *BlogSvc) SoftDeletePost(id int64) (*dto.PostResponse, error) {
+
+        }
+	        panic("not implemented") // TODO: Implement
+          func (blogsvc *BlogSvc) UpdateComment(comment *dto.UpdateCommentRequest) (*dto.CommentResponse, error) {
+
+	AddComment(comment *dto.AddCommentRequest) (*dto.CommentResponse, error)
+	UpdatePost(post *dto.UpdatePostRequest) (*dto.PostResponse, error)
+	UpdateComment(comment *dto.UpdateCommentRequest) (*dto.CommentResponse, error)
+	SoftDeletePost(id int64) (*dto.PostResponse, error)
+	SoftenDeleteComment(id int64) (*dto.CommentResponse, error)
 }
 
 func NewBlogSvc() *BlogSvc {
 	return &BlogSvc{
 		dao: dao.NewPostDao(),
 	}
+  
+  func (blogsvc *BlogSvc) GetPost(id int64) (*dto.PostResponse, error) {
+	  panic("not implemented") // TODO: Implement
+  }
+
+  func (blogsvc *BlogSvc) GetAllPosts() (*[]*dto.PostResponse, error) {
+	  panic("not implemented") // TODO: Implement
+  }
+
+  func (blogsvc *BlogSvc) CreatePost(post *dto.CreatePostRequest) (*dto.PostResponse, error) {
+	  panic("not implemented") // TODO: Implement
+  }
+
+  func (blogsvc *BlogSvc) AddComment(comment *dto.AddCommentRequest) (*dto.CommentResponse, error) {
+	  panic("not implemented") // TODO: Implement
+  }
+
+  func (blogsvc *BlogSvc) UpdatePost(post *dto.UpdatePostRequest) (*dto.PostResponse, error) {
+	  panic("not implemented") // TODO: Implement
+  }
 }
