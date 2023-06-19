2fdfde9aa86bcb5ff2690ba8aa146d743efa72f6
Matthew Slauson <matthew@slauson.io>
2023-06-19 13:25:48 -0500

    finish gets

--- dao/post.go
+++ dao/post.go
@@ -64,58 +64,51 @@ func (pd *PDao) ExistsByID(ID int64) (bool, error) {
 
 func (pd *PDao) GetPostByID(ID int64) (*siogeneric.BlogPost, error) {
 	sql := `SELECT id, title, body, posted_date, updated_date, deletion_date, soft_deleted FROM blog_posts WHERE id = $1 AND soft_deleted = false`
-	row := pd.db.QueryRowContext(ctx, sql, ID)
-	post, err := pd.scanPost(row)
+	rows, err := pd.db.QueryContext(ctx, sql, ID)
 	if err != nil {
 		return nil, err
 	}
+	defer rows.Close()
 
-	comments, err := pd.GetAllCommentsByPostID(ID)
-	if err != nil {
-		return nil, err
+	var post *siogeneric.BlogPost
+	for rows.Next() {
+		post, err := pd.scanPost(rows)
+		if err != nil {
+			return nil, err
+		}
+
+		comments, err := pd.GetAllCommentsByPostID(ID)
+		if err != nil {
+			return nil, err
+		}
+
+		post.Comments = comments
 	}
-
-	post.Comments = comments
-
 	return post, nil
 }
 
-// func (pd *PDao) GetAllPosts() (*[]siogeneric.BlogPost, error) {
-// 	sql := `SELECT
-// 			p.id, p.title, p.body, p.posted_date, p.updated_date, p.deletion_date, p.soft_deleted,
-// 			c.id, c.content, c.comment_date, c.soft_deleted, c.deletion_date where p.soft_deleted = false
-// 		FROM blog.post p
-// 		LEFT JOIN blog.comment c ON p.id = c.post_id`
-//
-// 	rows, err := pd.db.QueryContext(ctx, sql)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer rows.Close()
-//
-// 	posts := make(map[int64]*siogeneric.BlogPost)
-// 	for rows.Next() {
-// 		comment := &siogeneric.BlogComment{}
-// 		post := &siogeneric.BlogPost{}
-// 		if err := pd.scanPost(rows, post); err != nil {
-// 			return nil, err
-// 		}
-// 		if _, exists := posts[post.ID.Int64]; !exists {
-// 			posts[post.ID.Int64] = post
-// 		}
-// 		if err := pd.scanComment(rows, comment); err != nil {
-// 			return nil, err
-// 		}
-// 		posts[post.ID.Int64].Comments = append(posts[post.ID.Int64].Comments, comment)
-// 	}
-//
-// 	// Convert map values to slice
-// 	allPosts := make([]BlogPost, 0, len(posts))
-// 	for _, post := range posts {
-// 		allPosts = append(allPosts, *post)
-// 	}
-//
-// 	return allPosts, nil}
+func (pd *PDao) GetAllPosts() (*[]siogeneric.BlogPost, error) {
+	sql := `SELECT id, title, body, posted_date, updated_date, deletion_date, soft_deleted FROM blog_posts WHERE soft_deleted = false`
+	rows, err := pd.db.QueryContext(ctx, sql)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	posts := make([]siogeneric.BlogPost, 0)
+	for rows.Next() {
+		post, err := pd.scanPost(rows)
+		if err != nil {
+			return nil, err
+		}
+		comments, err := pd.GetAllCommentsByPostID(post.ID.Int64)
+		if err != nil {
+			return nil, err
+		}
+		post.Comments = comments
+		posts = append(posts, *post)
+	}
+	return &posts, nil
+}
 
 func (pd *PDao) GetAllCommentsByPostID(postID int64) (*[]siogeneric.BlogComment, error) {
 	sql := `SELECT id, content, comment_date, soft_deleted, deletion_date FROM blog_comments WHERE post_id = $1 AND soft_deleted = false`
@@ -128,9 +121,9 @@ func (pd *PDao) GetAllCommentsByPostID(postID int64) (*[]siogeneric.BlogComment,
 	return &comments, nil
 }
 
-func (pd *PDao) scanPost(row *sql.Row) (*siogeneric.BlogPost, error) {
+func (pd *PDao) scanPost(rows *sql.Rows) (*siogeneric.BlogPost, error) {
 	post := &siogeneric.BlogPost{}
-	err := row.Scan(&post.ID, &post.Title, &post.Body, &post.PostedDate,
+	err := rows.Scan(&post.ID, &post.Title, &post.Body, &post.PostedDate,
 		&post.UpdatedDate, &post.DeletionDate, &post.SoftDeleted)
 	if err != nil {
 		return nil, err
