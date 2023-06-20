27dccbff3e084f8e33c2661d67d53704b3fa8701
Matthew Slauson <matthew@slauson.io>
2023-06-19 20:22:51 -0500

    add validations

---
+++ utils/constants.go
@@ -0,0 +1,7 @@
+package utils
+
+const (
+	TITLE_TOO_LONG   = "title is too long.  please choose another title with no more than 100 characters."
+	BODY_TOO_LONG    = "post body is too long. please choose another post with no more than 10000 characters."
+	COMMENT_TOO_LONG = "comment is too long. please choose another comment with no more than 5000 characters."
+)
---
+++ utils/validations.go
@@ -0,0 +1,101 @@
+package utils
+
+import (
+	"gitea.slauson.io/blog/blog-ms/dto"
+	"gitea.slauson.io/slausonio/go-utils/sioUtils"
+	"gitea.slauson.io/slausonio/go-utils/sioerror"
+)
+
+type BlogValidation struct {
+	enc       *sioUtils.EncryptionUtil
+	validator *sioUtils.SioValidator
+}
+
+func NewValidator() *BlogValidation {
+	return &BlogValidation{
+		enc:       sioUtils.NewEncryptionUtil(),
+		validator: sioUtils.NewValidator(),
+	}
+}
+
+func (bv *BlogValidation) ValidateCreatePostRequest(req *dto.CreatePostRequest) error {
+	decryptedRequest := new(dto.CreatePostRequest)
+	*decryptedRequest = *req
+	if err := bv.enc.DecryptInterface(decryptedRequest); err != nil {
+		return sioerror.NewSioInternalServerError(sioUtils.DecryptionFailed)
+	}
+
+	if err := bv.validateTitle(decryptedRequest.Title); err != nil {
+		return err
+	}
+
+	if err := bv.validateBody(decryptedRequest.Body); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (bv *BlogValidation) ValidateUpdatePostRequest(req *dto.UpdatePostRequest) error {
+	decryptedRequest := new(dto.UpdatePostRequest)
+	*decryptedRequest = *req
+	if err := bv.enc.DecryptInterface(decryptedRequest); err != nil {
+		return sioerror.NewSioInternalServerError(sioUtils.DecryptionFailed)
+	}
+
+	if req.Title != "" {
+		if err := bv.validateTitle(decryptedRequest.Title); err != nil {
+			return err
+		}
+	}
+
+	if req.Body != "" {
+		if err := bv.validateBody(decryptedRequest.Body); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (bv *BlogValidation) ValidateAddCommentRequesdt(req *dto.AddCommentRequest) error {
+	content, err := bv.enc.Decrypt(req.Content)
+	if err != nil {
+		return sioerror.NewSioInternalServerError(sioUtils.DecryptionFailed)
+	}
+
+	return bv.validateCommentContent(content)
+}
+
+func (bv *BlogValidation) ValidateUpdateCommentRequesdt(req *dto.UpdateCommentRequest) error {
+	content, err := bv.enc.Decrypt(req.Content)
+	if err != nil {
+		return sioerror.NewSioInternalServerError(sioUtils.DecryptionFailed)
+	}
+
+	return bv.validateCommentContent(content)
+}
+
+func (bv *BlogValidation) validateTitle(title string) error {
+	if len(title) > 100 {
+		return sioerror.NewSioBadRequestError(TITLE_TOO_LONG)
+	}
+
+	return nil
+}
+
+func (bv *BlogValidation) validateBody(body string) error {
+	if len(body) > 100000 {
+		return sioerror.NewSioBadRequestError(BODY_TOO_LONG)
+	}
+
+	return nil
+}
+
+func (bv *BlogValidation) validateCommentContent(content string) error {
+	if len(content) > 100000 {
+		return sioerror.NewSioBadRequestError(COMMENT_TOO_LONG)
+	}
+
+	return nil
+}
