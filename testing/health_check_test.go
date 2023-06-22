51e5ab9660c1b192c727250ab162fb1e31a87fca
Matthew Slauson <matthew@slauson.io>
2023-06-21 19:10:19 -0500

    add health check

---
+++ testing/health_check_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+
+	"gitea.slauson.io/blog/blog-ms/handler"
+	"gitea.slauson.io/slausonio/go-testing/siotest"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHealthCheckRoot(t *testing.T) {
+	ts, _ := siotest.RunTestServer(t, handler.CreateRouter())
+	defer ts.Close()
+
+	req, err := http.NewRequest("GET", ts.URL+"/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	assert.Equalf(
+		t,
+		resp.StatusCode,
+		http.StatusOK,
+		"expected status code %d, got %d",
+		http.StatusOK,
+		resp.StatusCode,
+	)
+}
+
+func TestHealthCheckContextPath(t *testing.T) {
+	ts, _ := siotest.RunTestServer(t, handler.CreateRouter())
+	defer ts.Close()
+
+	req, err := http.NewRequest("GET", ts.URL+"/api/task", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	assert.Equalf(
+		t,
+		resp.StatusCode,
+		http.StatusOK,
+		"expected status code %d, got %d",
+		http.StatusOK,
+		resp.StatusCode,
+	)
+}
